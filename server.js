41fb690ac398ce25d118f576e68f77dd71457889
aldoprogrammer <aldobesma@gmail.com>
2024-01-16 19:19:26 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const port = 3001;
+
+// connect to db
+
+// data schema
+
+//data model
+
+//read route
+
+//create route
+
+//delete route
+
+//update route
+
+app.listen(port, function(){
+    console.log("Exxpress is running");
+});
