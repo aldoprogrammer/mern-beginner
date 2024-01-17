830738330f8ae42c6c3eb90e84fb406abd812433
aldoprogrammer <aldobesma@gmail.com>
2024-01-17 11:32:19 +0700

    update

--- server.js
+++ server.js
@@ -4,52 +4,56 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const port = 3001;
 
-//config
+// config
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 // connect to db
-
 mongoose.connect('mongodb+srv://aldobesma:BJrSaNJoxSz7s2Tm@cluster0.yr0w9xi.mongodb.net/newItems');
 
 
 // data schema
-const itemSchem = {
+const itemSchema = {
     title: String,
-    description: String
-}
+    description: String,
+};
 
+// data model
+const Item = mongoose.model('Item', itemSchema);
 
-//data model
-const Item = mongoose.model('Item', itemSchem);
-
-//read route
+// read route
 app.get('/items', (req, res) => {
     Item.find()
-    .then((item) => res.json(item))
-    .catch((err) => res.status(404).json("error", + err))
-})
+        .then((items) => res.json(items))
+        .catch((err) => res.status(404).json("Error" + err));
+});
 
-//create route
-
+// create route
 app.post('/newItem', (req, res) => {
-    const newItem = new Item(
-        {
-            title: req.body.title,
-            description: req.body.description
-        }
-    );
+    const newItem = new Item({
+        title: req.body.title,
+        description: req.body.description,
+    });
     newItem.save()
-    .then(item => console.log(item))
-    .catch(err => res.status(400).json("Error" + err))
-    
+        .then(item => res.json(item))
+        .catch(err => res.status(400).json("Error" + err));
+});
+
+// delete route
+app.delete('/delete/:id', (req, res) => {
+    Item.findByIdAndDelete(req.params.id)
+        .then(item => res.json(item))
+        .catch(err => res.status(400).json("Error" + err));
+});
+
+// update route
+app.put('/put/:id', (req, res) => {
+    Item.findByIdAndUpdate(req.body.id, req.body, { new: true })
+      .then(item => res.json(item))
+      .catch(err => res.status(400).json("Error" + err));
 })
 
-//delete route
-
-//update route
-
-app.listen(port, function(){
-    console.log("Exxpress is running");
+app.listen(port, function() {
+    console.log("Express is running");
 });
--- src/App.js
+++ src/App.js
@@ -54,8 +54,52 @@ function App() {
     .then(jsonRes => setItems(jsonRes))
     .catch(err => console.error(err))
   }, [items])
+
+  function deleteItem(id){
+    axios.delete('/delete/'+id)
+    alert(`delete item ${id}`)
+  }
+
+  const [isPut, setIsPut] = useState(false)
+  const [updatedItem, setUpdatedItem] = useState({
+    title: '',
+    description: '',
+    id: '',
+  })
+
+  function openUpdate(id) {
+    setIsPut(true);
+    setUpdatedItem((prevInput) => {
+      return {
+        ...prevInput,
+        _id: id,
+      };
+    });
+  }
+  
+
+  function updateItem(id) {
+    axios.put('/put/'+id, updatedItem)
+    alert('Item updated')
+    console.log(`item with id ${id} updated`);
+  }
+  
+  function handleUpdate(e) {
+    const {name, value} = e.target;
+    setUpdatedItem(prevInput => {
+      return (
+        {
+          ...prevInput,
+          [name]: value
+        }
+      )
+    })
+    console.log(updatedItem);
+  }
   return (
     <div className="App">
+      {!isPut ? 
+      (
       <div className='main'>
       <input 
        onChange={handleChange} 
@@ -73,14 +117,32 @@ function App() {
         onClick={addItem}
        >Add Item</button>
        </div>
+      ) : (<div className='main'>
+      <input 
+       onChange={handleUpdate} 
+       name='title' 
+       placeholder='title'
+       value={updatedItem.title} 
+       ></input>
+      <input 
+       onChange={handleUpdate} 
+       name='description' 
+       placeholder='description'
+       value={updatedItem.description} 
+       ></input>
+       <button onClick={() => 
+        openUpdate(updatedItem._id)}>Update</button>
+
+       </div>
+       )}
        {
         items.map(item => {
           return (
             <div key={item._id} style={{background: 'pink', width: '40%', margin: 'auto auto'}}>
             <p>{item.title}</p>
             <p>{item.description}</p>
-            <button>Delete</button>
-            <button>Update</button>
+            <button onClick={() => deleteItem(item._id)}>Delete</button>
+            <button onClick={() => openUpdate(updatedItem._id)}>Update</button>
             </div>
           )
         })
